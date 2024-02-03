3e9d607ca8a65f751f01e1601361992013172293
Leon200211 <leon200207@yandex.ru>
2024-02-03 15:21:00 +0300

    Базовые контроллеры

--- public_html/database/seeders/BlogCategoriesTableSeeder.php
+++ public_html/database/seeders/BlogCategoriesTableSeeder.php
@@ -25,7 +25,7 @@ class BlogCategoriesTableSeeder extends Seeder
         ];
 
         for ($i = 1; $i <= 10; $i++) {
-            $cName = "Кутегория №{$i}";
+            $cName = "Категория №{$i}";
             $parentId = ($i > 4) ? rand(1, 4) : 1;
 
             $categories[] = [
